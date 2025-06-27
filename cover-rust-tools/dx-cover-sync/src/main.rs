8e883852b6a69f1a9f4dc9f445b9b9bc2a3a98d7
Hsiao-Ting Yu <sst.dreams@gmail.com>
2025-06-28 02:35:11 +0800

    feat: update cover sync tools

--- cover-rust-tools/dx-cover-sync/src/main.rs
+++ cover-rust-tools/dx-cover-sync/src/main.rs
@@ -1,48 +1,182 @@
-use std::io::Cursor;
+use std::collections::{HashMap, HashSet};
 use std::path::PathBuf;
 
 use anyhow::ensure;
 use clap::Parser;
-use serde::Deserialize;
-use tokio::fs::{read_to_string, File};
-use tokio::io::copy;
-use tokio::time::{sleep, Duration};
+use serde::{Deserialize, Serialize};
+use tokio::fs::{File, read_to_string};
+//use tokio::io::copy;
+//use tokio::time::{Duration, sleep};
 
 #[derive(Deserialize, Debug)]
 struct SongEntry {
-    title: String,
+    artist: String,
+    catcode: String,
     image_url: String,
+    release: String,
+    #[serde(default)]
+    lev_mas: Option<String>,
+    #[serde(default)]
+    dx_lev_mas: Option<String>,
+    sort: String,
+    title: String,
+    title_kana: String,
+    version: String,
+}
+
+#[derive(Deserialize, Serialize, Debug)]
+struct OutputInfo {
+    artist: String,
+    title_kana: String,
 }
 
 #[derive(Parser, Debug)]
+#[command(
+    about = "Cover image and song info sync tool. Merge existing song info JSON and folder using data from official site or archived folder."
+)]
 struct Cli {
-    #[arg(value_hint = clap::ValueHint::DirPath)]
-    path: PathBuf,
+    #[arg(long, value_hint=clap::ValueHint::FilePath, help="Use offline archive folder with `maimai_songs.json` and cover JPEG files")]
+    cover_archive: Option<PathBuf>,
+    #[arg(
+        long,
+        action,
+        help = "If true, it will use versions list file instead of internal lv file. It is useful to include removed songs."
+    )]
+    versions: bool,
+    #[arg(value_hint = clap::ValueHint::FilePath, help="Internal lv or versions lookup file, used to filter songs to be included")]
+    json_path: PathBuf,
+    #[arg(value_hint = clap::ValueHint::FilePath, help="Path for the cover image files to be merged")]
+    cover_path: PathBuf,
+    #[arg(value_hint = clap::ValueHint::FilePath, help="Path for the output JSON file with artist and other info to be merged")]
+    info_json_path: PathBuf,
+}
+
+fn catcode_to_category(catcode: &str) -> anyhow::Result<i32> {
+    match catcode {
+        "POPS＆アニメ" => Ok(1),
+        "niconico＆ボーカロイド" => Ok(2),
+        "東方Project" => Ok(3),
+        "ゲーム＆バラエティ" => Ok(4),
+        "maimai" => Ok(5),
+        "オンゲキ＆CHUNITHM" => Ok(6),
+        _ => Err(anyhow::anyhow!("Unknown catcode: {}", catcode)),
+    }
+}
+
+fn song_title_fixes(from_json: &str) -> &str {
+    match from_json {
+        "Bad Apple!! feat. nomico" => "Bad Apple!! feat nomico",
+        _ => from_json,
+    }
 }
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     let cli = Cli::parse();
-    ensure!(cli.path.is_dir(), "Input path should be a dir");
-    let json_contents = read_to_string(cli.path.join("maimai_songs.json")).await?;
-    let songs: Vec<SongEntry> = serde_json::from_str(&json_contents)?;
+    ensure!(cli.json_path.is_file(), "Input path should be a file");
+    if cli.cover_archive.is_some() {
+        ensure!(
+            cli.cover_archive.as_ref().unwrap().is_dir(),
+            "Cover archive path should be a dir"
+        );
+    }
+    ensure!(cli.cover_path.is_dir(), "Cover path should be a dir");
+    ensure!(
+        cli.info_json_path.is_file(),
+        "Info JSON path should be a file"
+    );
+
+    let json_contents = read_to_string(cli.json_path).await?;
+    let mut variants = HashSet::<String>::new();
+    if cli.versions {
+        let versions_lookup: Vec<Vec<String>> = serde_json::from_str(&json_contents)?;
+        // flatten versions_lookup and append into variants
+        for version in versions_lookup {
+            variants.extend(version);
+        }
+    } else {
+        let internal_lv_lookup: HashMap<String, f64> = serde_json::from_str(&json_contents)?;
+        for key in internal_lv_lookup.keys() {
+            if key.len() > 2 {
+                variants.insert(key[..key.len() - 2].to_string());
+            }
+        }
+    }
+    let mut info = serde_json::from_str::<HashMap<String, OutputInfo>>(
+        &read_to_string(&cli.info_json_path).await?,
+    )?;
+
     // The certificate chain on the site is broken :(
-    let client = reqwest::Client::builder().danger_accept_invalid_certs(true).build()?;
+    let client = reqwest::Client::builder()
+        .danger_accept_invalid_certs(true)
+        .build()?;
+    let songs = match cli.cover_archive {
+        Some(ref cover_archive) => {
+            // Use the cover archive
+            let archive_json = read_to_string(cover_archive.join("maimai_songs.json")).await?;
+            serde_json::from_str::<Vec<SongEntry>>(&archive_json)?
+        }
+        None => {
+            // Fetch from the official site
+            client
+                .get("https://maimai.sega.jp/data/maimai_songs.json")
+                .send()
+                .await?
+                .json::<Vec<SongEntry>>()
+                .await?
+        }
+    };
+
     for song in songs.iter() {
-        let title = &song.title;
-        let filename = &song.image_url;
-        let image_path = cli.path.join(filename);
-        if image_path.exists() {
-            println!("{title} ({filename}) file exists, skipped");
+        let title = song_title_fixes(&song.title);
+        if song.catcode == "宴会場" {
             continue;
         }
-        println!("Downloading {title} ({filename})...");
-        let url = format!("https://maimaidx.jp/maimai-mobile/img/Music/{filename}");
-        let resp = client.get(url).send().await?.error_for_status()?;
-        let mut file = File::create(image_path).await?;
-        let mut content = Cursor::new(resp.bytes().await?);
-        copy(&mut content, &mut file).await?;
-        sleep(Duration::from_secs(1)).await;
+        let category = catcode_to_category(song.catcode.as_str())?;
+        if !(&song.lev_mas).is_none() {
+            let variant_key = format!("{category}_{title}_f");
+            variants.remove(&variant_key);
+        }
+        if !(&song.dx_lev_mas).is_none() {
+            let variant_key = format!("{category}_{title}_t");
+            variants.remove(&variant_key);
+        }
+        // Make a sha256 with format!("{category}_{title}")
+        let info_key = format!("{category}_{title}");
+        if let Some(output_info) = info.get_mut(&info_key) {
+            output_info.artist = song.artist.clone();
+            output_info.title_kana = song.title_kana.clone();
+        } else {
+            info.insert(
+                info_key,
+                OutputInfo {
+                    artist: song.artist.clone(),
+                    title_kana: song.title_kana.clone(),
+                },
+            );
+        }
+        let hash = format!("{category}_{title}");
+        //     let filename = &song.image_url;
+        //     let image_path = cli.path.join(filename);
+        //     if image_path.exists() {
+        //         println!("{title} ({filename}) file exists, skipped");
+        //         continue;
+        //     }
+        //     println!("Downloading {title} ({filename})...");
+        //     let url = format!("https://maimaidx.jp/maimai-mobile/img/Music/{filename}");
+        //     let resp = client.get(url).send().await?.error_for_status()?;
+        //     let mut file = File::create(image_path).await?;
+        //     let mut content = Cursor::new(resp.bytes().await?);
+        //     copy(&mut content, &mut file).await?;
+        //     sleep(Duration::from_secs(1)).await;
+
+        // Write back updated info JSON
+        let info_json = serde_json::to_string_pretty(&info)?;
+        tokio::fs::write(&cli.info_json_path, info_json).await?;
+    }
+    println!("Done. Variants left not found in the source:");
+    for variant in variants {
+        println!("{variant}");
     }
     Ok(())
 }
